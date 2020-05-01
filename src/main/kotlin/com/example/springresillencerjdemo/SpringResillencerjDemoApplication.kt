a296084ac6c573c19447ef8ef0bd57b05ebc433f
vikas <vikas.on@gmail.com>
2020-05-01 21:13:13 +0700

    add resilience

--- src/main/kotlin/com/example/springresillencerjdemo/SpringResillencerjDemoApplication.kt
+++ src/main/kotlin/com/example/springresillencerjdemo/SpringResillencerjDemoApplication.kt
@@ -1,18 +1,43 @@
 package com.example.springresillencerjdemo
 
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+import io.github.resilience4j.timelimiter.TimeLimiterConfig
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder
+import org.springframework.cloud.client.circuitbreaker.Customizer
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import java.util.function.Function
+
 
 @SpringBootApplication
-class SpringResilienceDemoApplication
+class SpringResilienceDemoApplication {
+
+    @Bean
+    fun defaultCustomizer(): Customizer<ReactiveResilience4JCircuitBreakerFactory> {
+        return Customizer {
+            it.configureDefault {
+                Resilience4JConfigBuilder.Resilience4JCircuitBreakerConfiguration()
+                        .apply {
+                            id = "default"
+                            circuitBreakerConfig = CircuitBreakerConfig.ofDefaults()
+                            timeLimiterConfig = TimeLimiterConfig.ofDefaults()
+
+                        }
+            }
+        }
+    }
+}
 
 @RestController
 class SomeController {
@@ -27,9 +52,8 @@ class SomeController {
 
 @Service
 class SomeService {
-
     fun process(id: Int): Mono<String> {
-        return if (id < 1) throw IllegalArgumentException("Illegal id")
+        return if (id < 1) Mono.error{ IllegalArgumentException("meaw!! I am throwing error") }
         else Mono.just("Id Found!")
     }
 }
